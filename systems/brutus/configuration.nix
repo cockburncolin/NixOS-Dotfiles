0c2e4398b6d96eec24dc28d2908f24347612f56e
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-10-26 19:11:05 -0700

    Add laptop

---
+++ systems/brutus/configuration.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  custom = {
+    developmentUtils.enable = true;
+    emacs.enable = false;
+    games.enable = false;
+    windowManager.enable = true;
+    windowManager.niri.enable = false;
+    windowManager.hyprland.enable = true;
+    virt.enable = false;
+  };
+
+  networking.hostName = "brutus"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  environment.systemPackages = with pkgs; [
+    neovim
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
